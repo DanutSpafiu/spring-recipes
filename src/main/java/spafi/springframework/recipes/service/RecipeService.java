159324e22ec191f505500d3a126599839ef12da2
DanutSpafiu <spafiu.spafiu@yahoo.com>
2026-05-16 18:33:33 +0300

    some recipe service, not done

--- src/main/java/spafi/springframework/recipes/service/RecipeService.java
+++ src/main/java/spafi/springframework/recipes/service/RecipeService.java
@@ -1,8 +1,12 @@
 package spafi.springframework.recipes.service;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import spafi.springframework.recipes.model.Recipe;
 
+import java.util.List;
+
 public interface RecipeService {
+
+    List<Recipe> getAllRecipes();
+
+    Recipe getRecipeById(Long id);
 }
---
+++ src/main/java/spafi/springframework/recipes/service/RecipeServiceImpl.java
@@ -0,0 +1,28 @@
+package spafi.springframework.recipes.service;
+
+import org.springframework.stereotype.Service;
+import spafi.springframework.recipes.model.Recipe;
+import spafi.springframework.recipes.repositories.RecipeRepository;
+
+import java.util.List;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public List<Recipe> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
+
+    @Override
+    public Recipe getRecipeById(Long id) {
+        return recipeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Recipe not found: " + id));
+    }
+}

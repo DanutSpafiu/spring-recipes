6de5542be19dd4717cc3ec86c419fdf47f37779f
DanutSpafiu <spafiu.spafiu@yahoo.com>
2026-05-16 18:43:17 +0300

    Replaced Collection with RecipeCollection

--- src/main/java/spafi/springframework/recipes/repositories/CollectionRepository.java
+++ src/main/java/spafi/springframework/recipes/repositories/CollectionRepository.java
@@ -1,8 +1,9 @@
 package spafi.springframework.recipes.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import spafi.springframework.recipes.model.RecipeCollection;
 
 import java.util.Collection;
 
-public interface CollectionRepository extends JpaRepository<Collection, Long> {
+public interface CollectionRepository extends JpaRepository<RecipeCollection, Long> {
 }

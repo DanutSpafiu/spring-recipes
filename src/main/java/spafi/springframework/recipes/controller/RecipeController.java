e340b62d56f68e92cd052d523a37f81497075d1d
DanutSpafiu <spafiu.spafiu@yahoo.com>
2026-05-17 20:45:12 +0300

    Added first version of recipe controller with basic CRUD operations

---
+++ src/main/java/spafi/springframework/recipes/controller/RecipeController.java
@@ -0,0 +1,34 @@
+package spafi.springframework.recipes.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import spafi.springframework.recipes.model.Recipe;
+import spafi.springframework.recipes.service.RecipeService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/recipes")
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @GetMapping
+    public List<Recipe> getAllRecipes() {
+        return recipeService.getAllRecipes();
+    }
+
+    @GetMapping("/{id}")
+    public Recipe getRecipeById(Long id) {
+        return recipeService.getRecipeById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Recipe addNewRecipe(@RequestBody Recipe recipe) {
+        return recipeService.addNewRecipe(recipe);
+    }
+}

5d9d76a9a9757ab26a5b27deee349e0a098312a6
DanutSpafiu <spafiu.spafiu@yahoo.com>
2026-05-17 21:03:12 +0300

    minor changes

--- src/main/java/spafi/springframework/recipes/service/UserService.java
+++ src/main/java/spafi/springframework/recipes/service/UserService.java
@@ -2,4 +2,5 @@ package spafi.springframework.recipes.service;
 
 public interface UserService {
     void addFavoriteRecipe(Long userId, Long recipeId);
+    void rateRecipe(Long userId, Long recipeId, int rating);
 }
